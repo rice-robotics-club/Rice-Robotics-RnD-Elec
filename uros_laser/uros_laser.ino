918bffcb60e8504a93b7a1961da1b3799620c846
at231-bit <at231@rice.edu>
2025-10-16 20:03:10 -0500

    Added matrix operations

--- uros_laser/uros_laser.ino
+++ uros_laser/uros_laser.ino
@@ -1,7 +1,12 @@
 #include "laser.h"
 #include "imu.h"
+# include <Eigen.h>
+#include<Eigen/Dense>
 
 #define LED_BUILTIN 2
+
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -15,9 +20,32 @@ void setup() {
 }
 
 
+
+Eigen::Matrix4d transformationTarget = Matrix4d::Identity();
+Eigen::Matrix4d transformationRobot = Matrix4d::Identity();
+
 void loop() {
   updateDistance();
   updateIMU();
+
+
+  Eigen::Matrix3d Rz;
+  Rz << cos(yaw), -sin(yaw), 0,
+        sin(yaw),  cos(yaw), 0,
+        0,   0,  1;
+
+  Eigen::Matrix3d Ry;
+  Ry << cos(pitch), 0, sin(pitch),
+        0,  1, 0,
+        -sin(pitch), 0, cos(pitch);
+
+  Eigen::Matrix3d R = Rz*Ry;
+  Eigen::Vector3d Point(distance, 0, 0);
+
+  Eigen::Matrix4d T = Matrix4d::Identity();
+  T.block<3, 3>(0,0) = R;
+  T.block<3, 1>(0,3) = Point;
+
 
   Serial.print("Distance: ");
   Serial.println(distance);
