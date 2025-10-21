3e9eacf79c50408603b36ba28926577b60951a1a
at231-bit <at231@rice.edu>
2025-10-21 18:25:51 -0500

    Created function for calculating Transformation Matrix

--- uros_laser/uros_laser.ino
+++ uros_laser/uros_laser.ino
@@ -1,7 +1,12 @@
 #include "laser.h"
 #include "imu.h"
+# include <Eigen.h>
+#include<Eigen/Dense>
 
 #define LED_BUILTIN 2
+
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -14,10 +19,44 @@ void setup() {
   delay(2000);
 }
 
+Eigen::Matrix4d transformationTarget = Matrix4d::Identity();
+Eigen::Matrix4d transformationRobot = Matrix4d::Identity();
 
-void loop() {
+Matrix4d transformationMatrix(){
   updateDistance();
   updateIMU();
+
+  double cyaw = cos(yaw * M_PI / 180.0);
+  double syaw = sin(yaw * M_PI / 180.0);
+  double cpitch = cos(pitch * M_PI / 180.0);
+  double spitch = sin(pitch * M_PI / 180.0);
+
+  Eigen::Matrix3d Rz;
+  Rz << cyaw, -syaw, 0,
+        syaw,  cyaw, 0,
+        0,   0,  1;
+
+  Eigen::Matrix3d Ry;
+  Ry << cpitch, 0, spitch,
+        0,  1, 0,
+        -spitch, 0, cpitch;
+
+  Eigen::Matrix3d R = Rz*Ry;
+
+  Eigen::Vector3d Point(distance, 0, 0);
+
+  Eigen::Matrix4d T = Matrix4d::Identity();
+  T.block<3, 3>(0,0) = R;
+  T.block<3, 1>(0,3) = Point;
+
+  return T;
+}
+
+
+void loop() {
+  Matrix4d T = transformationMatrix();
+  
+
 
   Serial.print("Distance: ");
   Serial.println(distance);
