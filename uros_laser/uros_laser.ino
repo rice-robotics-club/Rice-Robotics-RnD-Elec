f1a4e646bb624457b24a7f4c6df47957d1514c24
Alexis_ta88 <at231@rice.edu>
2025-10-21 18:34:10 -0500

    Update uros_laser.ino

--- uros_laser/uros_laser.ino
+++ uros_laser/uros_laser.ino
@@ -19,25 +19,28 @@ void setup() {
   delay(2000);
 }
 
-
-
 Eigen::Matrix4d transformationTarget = Matrix4d::Identity();
 Eigen::Matrix4d transformationRobot = Matrix4d::Identity();
 
-void loop() {
+
+Matrix4d transformationMatrix(){
   updateDistance();
   updateIMU();
 
+  double cyaw = cos(yaw * M_PI / 180.0);
+  double syaw = sin(yaw * M_PI / 180.0);
+  double cpitch = cos(pitch * M_PI / 180.0);
+  double spitch = sin(pitch * M_PI / 180.0);
 
   Eigen::Matrix3d Rz;
-  Rz << cos(yaw), -sin(yaw), 0,
-        sin(yaw),  cos(yaw), 0,
+  Rz << cyaw, -syaw, 0,
+        syaw,  cyaw, 0,
         0,   0,  1;
 
   Eigen::Matrix3d Ry;
-  Ry << cos(pitch), 0, sin(pitch),
+  Ry << cpitch, 0, spitch,
         0,  1, 0,
-        -sin(pitch), 0, cos(pitch);
+        -spitch, 0, cpitch;
 
   Eigen::Matrix3d R = Rz*Ry;
   Eigen::Vector3d Point(distance, 0, 0);
@@ -45,8 +48,14 @@ void loop() {
   Eigen::Matrix4d T = Matrix4d::Identity();
   T.block<3, 3>(0,0) = R;
   T.block<3, 1>(0,3) = Point;
+  
+  return T;
+}
 
 
+void loop() {
+  Matrix4d T = transformationMatrix();
+  
   Serial.print("Distance: ");
   Serial.println(distance);
 
@@ -58,3 +67,4 @@ void loop() {
 
   delay(100);
 }
+
