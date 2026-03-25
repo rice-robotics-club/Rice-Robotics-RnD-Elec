b4f68dc091361a6e3bf2974acab56b936f358c79
Alexis_ta88 <at231@rice.edu>
2026-03-24 19:00:21 -0500

    Refactor distance update logic in laser.cpp

--- uros_laser/laser.cpp
+++ uros_laser/laser.cpp
@@ -10,42 +10,38 @@ float distance;
 
 void setupDistance() {
   Serial1.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
-  Serial1.write(boot_buffer, sizeof(boot_buffer));
-  delay(1000);  // wait 1 second for sensor to boot
 }
 
-int laser_index = 0;
-
 void updateDistance() {
-  laser_index++;
-  while (Serial1.available() > 0) {
-    if (laser_index % 5 != 0) return;
-    
-    if (Serial1.available() >= 11){
-      for (int i = 0; i < 11; i++) {  // wait until full frame received
-        data[i] = Serial1.read();
-      }
+  static unsigned long lastTrigger = 0;
+  if (millis() - lastTrigger >= 200) {
+    Serial1.write(boot_buffer, 4);
+    lastTrigger = millis();
+  }
+  while (Serial1.available()> 0 && Serial1.peek() != 0x80){
+    Serial1.read();
+  }
+  if (Serial1.available() >= 11) {  // wait until full frame received
+    for (int i = 0; i < 11; i++) {
+      data[i] = Serial1.read();
+    }
 
-      unsigned char Check = 0;
-      for (int i = 0; i < 10; i++) {
-        Check += data[i];
-      }
-      Check = ~Check + 1;
+    unsigned char Check = 0;
+    for (int i = 0; i < 10; i++) {
+      Check += data[i];
+    }
+    Check = ~Check + 1;
 
-      if (data[10] == Check) {
-        if (data[3] == 'E' && data[4] == 'R' && data[5] == 'R') {
-          distance = -1.0;
-        } else {
-          distance = 0.0;
-          distance = (data[3] - 0x30) * 100 +
-                    (data[4] - 0x30) * 10 +
-                    (data[5] - 0x30) * 1 +
-                    (data[7] - 0x30) * 0.1 +
-                    (data[8] - 0x30) * 0.01 +
-                    (data[9] - 0x30) * 0.001;
-        }
-      } else {
+    if (data[10] == Check) {
+      if (data[3] == 'E' && data[4] == 'R' && data[5] == 'R') {
         distance = -1.0;
+      } else {
+        distance = (data[3] - 0x30) * 100 +
+                   (data[4] - 0x30) * 10 +
+                   (data[5] - 0x30) * 1 +
+                   (data[7] - 0x30) * 0.1 +
+                   (data[8] - 0x30) * 0.01 +
+                   (data[9] - 0x30) * 0.001;
       }
     }
   }
